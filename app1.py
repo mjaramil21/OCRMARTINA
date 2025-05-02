c1f6ca81e38ef1b29ce3d29a6a069a3d32445bd6
mjaramil21 <mjaramil21@eafit.edu.co>
2025-05-01 22:57:30 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,30 +4,51 @@ import numpy as np
 import pytesseract
 from PIL import Image
 
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://i.pinimg.com/736x/5d/30/69/5d30696197589005cb50ce40d0f13c37.jpg");
+        background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-st.title("Reconocimiento óptico de Caracteres")
+# Título en rojo
+st.markdown("<h1 style='color: red;'>FNSM OCR</h1>", unsafe_allow_html=True)
 
-img_file_buffer = st.camera_input("Toma una Foto")
+st.write('A veces, es dificil leer las fotos para encontrar pistas. '
+         ' Este es un piloto de reconocimiento óptico de caracteres que '
+         ' nuesto Spidey developer (Ganke) diseñó. '
+        )
 
+# Subheader de la cámara en azul
+st.markdown("<h3 style='color: blue;'>Pongámoslo a prueba: toma una foto ;)</h3>", unsafe_allow_html=True)
+img_file_buffer = st.camera_input("")
+
+# Sidebar con header en azul
 with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
-
+    st.markdown("<h3 style='color: blue;'>Opciones</h3>", unsafe_allow_html=True)
+    filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
 if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
+    # Leer imagen del buffer
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    
+
     if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
-    else:
-         cv2_img= cv2_img
-    
-        
+        cv2_img = cv2.bitwise_not(cv2_img)
+
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
-    st.write(text) 
-    
+    text = pytesseract.image_to_string(img_rgb)
+
+    # Mostrar resultado
+    st.markdown("<h3 style='color: blue;'>Texto Detectado</h3>", unsafe_allow_html=True)
+    st.write(text)
 
 
     
